d71e647624808bc67bdf8f20ecbde5a4aa724142
anastasiaShpakava <shpakavaa@gmail.com>
2020-10-07 17:26:06 +0300

    added onClickListener (new View.OnclickListener) methods for start/stop service

--- app/src/main/java/com/company/androidservice/MainActivity.java
+++ app/src/main/java/com/company/androidservice/MainActivity.java
@@ -2,13 +2,35 @@ package com.company.androidservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonStart;
+    private Button buttonStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStart = findViewById(R.id.button_start);
+        buttonStop = findViewById(R.id.button_stop);
+        final Intent intent = new Intent(this, PlayService.class);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(intent);
+            }
+        });
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(intent);
+            }
+        });
     }
 }
