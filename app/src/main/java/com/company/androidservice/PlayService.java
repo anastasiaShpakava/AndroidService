d206bcf484e41616a6cf8e294f92a3643786d612
anastasiaShpakava <shpakavaa@gmail.com>
2020-10-07 17:10:11 +0300

    added PlayService.java, buttons in activity_main.xmlfor start/stop service

---
+++ app/src/main/java/com/company/androidservice/PlayService.java
@@ -0,0 +1,41 @@
+package com.company.androidservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class PlayService extends Service {
+    private MediaPlayer mediaPlayer;
+
+    public PlayService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+      return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this, "Service created", Toast.LENGTH_SHORT).show();
+        mediaPlayer = MediaPlayer.create(this,R.raw.eminem);
+        mediaPlayer.setLooping(false);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Service is started",Toast.LENGTH_SHORT ).show();
+        mediaPlayer.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service stopped",Toast.LENGTH_SHORT).show();
+        mediaPlayer.stop();
+    }
+}
