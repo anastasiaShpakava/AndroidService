150f54ab64c0acbf50f8d4d16297c0817fcff3bc
anastasiaShpakava <shpakavaa@gmail.com>
2020-10-08 13:34:52 +0300

    added onPause(), onResume() methods

--- app/src/main/java/com/company/androidservice/MainActivity.java
+++ app/src/main/java/com/company/androidservice/MainActivity.java
@@ -3,22 +3,28 @@ package com.company.androidservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonStart;
     private Button buttonStop;
+    private Button buttonPause;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonStart = findViewById(R.id.button_start);
         buttonStop = findViewById(R.id.button_stop);
-        final Intent intent = new Intent(this, PlayService.class);
+        buttonStart = findViewById(R.id.button_start);
+        buttonPause = findViewById(R.id.button_pause);
+
+      intent = new Intent(this, PlayService.class);
 
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +38,25 @@ public class MainActivity extends AppCompatActivity {
                 stopService(intent);
             }
         });
+        buttonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onStop();
+            }
+        });
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        stopService(intent);
+        Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopService(intent);
     }
 }
